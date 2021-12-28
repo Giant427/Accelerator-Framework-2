a7d9c9f5d27ec83796d89b2a75f6f9172188eea9
Giant <83135006+Giant427@users.noreply.github.com>
2021-12-28 13:06:48 +0530

    Create GunResourcesHandler.lua

---
+++ src/ReplicatedStorage/AcceleratorFramework/Modules/GunResourcesHandler.lua
@@ -0,0 +1,56 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-------------
+-- FOLDERS --
+-------------
+
+local ReplicatedStorageFolder = ReplicatedStorage:WaitForChild("AcceleratorFramework")
+local ResourcesFolder = ReplicatedStorageFolder:WaitForChild("GunResources")
+
+---------------------------
+-- GUN RESOURCES HANDLER --
+---------------------------
+
+local GunResourcesHandler = {}
+
+-- Get a particular resource
+
+function GunResourcesHandler:GetResource(RESOURCE_TYPE, GUN_NAME)
+    local ResourceContainer = ResourcesFolder:FindFirstChild(RESOURCE_TYPE)
+
+    if not ResourceContainer then return end
+
+    local Resource = ResourceContainer:FindFirstChild(GUN_NAME)
+
+    if ResourceContainer:IsA("ModuleScript") then
+        Resource = require(ResourceContainer)[GUN_NAME]
+    end
+
+    return Resource
+end
+
+-- Get gun resources
+
+function GunResourcesHandler:GetGunRecources(GUN_NAME)
+    local RecoilPattern = self:GetResource("RecoilPattern", GUN_NAME)
+    local ViewmodelOffset = self:GetResource("ViewmodelOffset", GUN_NAME)
+    local Comms = self:GetResource("Comms", GUN_NAME)
+    local Stats = self:GetResource("Stats", GUN_NAME)
+    local Sounds = self:GetResource("Sounds", GUN_NAME)
+    local VisualEffects = self:GetResource("VisualEffects", GUN_NAME)
+    local Animations = self:GetResource("Animations", GUN_NAME)
+    local Model = self:GetResource("Models", GUN_NAME)
+    local Shell = self:GetResource("Shells", GUN_NAME)
+
+    return RecoilPattern, ViewmodelOffset, Comms, Stats, Sounds, VisualEffects, Animations, Model, Shell
+end
+
+-- Get Viewmodel
+
+function GunResourcesHandler:GetViewmodel()
+    local Viewmodel = ResourcesFolder:FindFirstChild("Viewmodel")
+
+    return Viewmodel
+end
+
+return GunResourcesHandler
